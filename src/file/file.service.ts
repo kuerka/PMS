45642ecdc53387a533e9cc33a5308274f93482b4
luoluo <2796673723@qq.com>
2025-02-19 23:01:15 +0800

    chore：修改文件上传存储方式

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,12 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { File as FileEntity } from './file.entity';
 import { Repository } from 'typeorm';
 import * as fs from 'fs';
 import * as path from 'path';
+import { ulid } from 'ulid';
 
 @Injectable()
 export class FileService {
@@ -17,24 +15,40 @@ export class FileService {
   ) {}
 
   async create(file: Express.Multer.File) {
-    if (!fs.existsSync(this.uploadDir)) fs.mkdirSync(this.uploadDir);
-    const filename = Buffer.from(file.originalname, 'latin1').toString('utf8');
-    const filesize = file.size as number;
+    return await this.fileRepository.manager.transaction(async (manager) => {
+      let filename = file.originalname;
+      filename = Buffer.from(filename, 'latin1').toString('utf8');
 
-    const filepath = path.join(this.uploadDir, filename);
-    fs.writeFileSync(filepath, file.buffer);
+      const fileDir = path.join(this.uploadDir, ulid());
+      if (!fs.existsSync(fileDir)) fs.mkdirSync(fileDir, { recursive: true });
+      const filepath = path.join(fileDir, filename);
 
-    const files = [{ filename, filepath, filesize }];
-    const res = await this.fileRepository.save(files);
+      const files = new FileEntity();
+      files.path = filepath;
+      files.type = '中标通知书';
+      files.name = filename;
+      files.size = file.size;
+      files.updatedAt = new Date();
+      files.createdAt = new Date();
+      await manager.save(files);
 
-    return res;
+      fs.writeFileSync(filepath, file.buffer);
+
+      return files;
+    });
   }
 
   async delete(id: number) {
-    const file = await this.fileRepository.findOneBy({ id });
-    if (!file) return;
-    await this.fileRepository.delete({ id });
-    fs.rmSync(file.filepath, { force: true, recursive: true });
-    return file;
+    return await this.fileRepository.manager.transaction(async (manager) => {
+      const file = await manager.findOneBy(FileEntity, { id });
+      if (!file) return;
+      await manager.delete(FileEntity, { id });
+
+      if (file.path) {
+        const dirName = path.dirname(file.path);
+        fs.rmSync(dirName, { force: true, recursive: true });
+      }
+      return file;
+    });
   }
 }
