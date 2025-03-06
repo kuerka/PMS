38f9d1533c99e2aebbd2319c8fc26195c0217692
zhenglibin <2796673723@qq.com>
2025-03-06 17:00:45 +0800

    chore: 修改查询语句使用CTE

--- src/contract/services/contract.service.ts
+++ src/contract/services/contract.service.ts
@@ -6,6 +6,10 @@ import { PaginationDto } from '@/pagination/pagination.dto';
 import { CostFormService } from '@/cost-form/cost-form.service';
 import { PerformanceService } from './performance.service';
 import { PaymentMethodService } from './payment-method.service';
+import { ProductionCostForm } from '@/cost-form/entities/cost-form.entity';
+import { CollaborationCompany } from '@/cost-form/entities/collaboration-company.entity';
+import { CollaborationCompanyInvoice } from '@/cost-form/entities/collaboration-company-invoice.entity';
+import { CollaborationCompanyPayment } from '@/cost-form/entities/collaboration-company-payment.entity';
 
 @Injectable()
 export class ContractService {
@@ -73,76 +77,55 @@ export class ContractService {
   }
 
   async getAccumulatedAmount(limit: number = 20, offset: number = 2000) {
-    const LimitAndSkipQuery = this.dataSource.manager
+    const contractQuery = this.dataSource
       .createQueryBuilder()
-      .subQuery()
-      .select('id')
-      .from(
-        (qb) =>
-          qb
-            .subQuery()
-            .select()
-            .from(Contract, 'ccl')
-            .limit(limit)
-            .offset(offset),
-        'ccl',
+      .select()
+      .from(Contract, 'c')
+      .skip(offset)
+      .take(limit);
+
+    const companyQuery = this.dataSource
+      .createQueryBuilder()
+      .select('cct_c.id', 'cId')
+      .from('cct_c', 'cct_c')
+      .leftJoin(ProductionCostForm, 'pcf', 'pcf.contract_id = cct_c.id')
+      .leftJoin(
+        CollaborationCompany,
+        'cc',
+        'cc.production_cost_form_id = pcf.id',
       );
 
-    const cctSubQuery = this.dataSource.manager
+    const InvoiceQuery = this.dataSource
       .createQueryBuilder()
       .subQuery()
-      .select('c.id', 'cId')
-      .addSelect('cc.id', 'ccId')
-      .from(Contract, 'c')
-      .leftJoin('production_cost_form', 'pcf', 'c.id = pcf.contract_id')
-      .leftJoin(
-        'collaboration_company',
-        'cc',
-        'pcf.id = cc.production_cost_form_id',
-      )
-      .where((qb) => {
-        const subQUery = qb
-          .subQuery()
-          .select('id')
-          .from(LimitAndSkipQuery.getQuery(), 't2');
-        return `c.id IN ${subQUery.getQuery()}`;
-      });
-
-    const invoiceSubQuery = this.dataSource.manager
-      .createQueryBuilder()
-      .subQuery()
-      .select('t.cId', 'cId')
+      .select('cte_cc.cId', 'cId')
       .addSelect('SUM(cci.invoice_amount)', 'ia')
-      .from(cctSubQuery.getQuery(), 't')
-      .leftJoin(
-        'collaboration_company_invoice',
-        'cci',
-        'cci.company_id = t.ccId',
-      )
-      .groupBy('t.cId');
-    const paymentSubQuery = this.dataSource.manager
+      .from('cte_cc', 'cte_cc')
+      .leftJoin(CollaborationCompanyInvoice, 'cci', 'cci.company_id=cte_cc.cId')
+      .groupBy('cte_cc.cId');
+
+    const PaymentQuery = this.dataSource
       .createQueryBuilder()
       .subQuery()
-      .select('t.cId', 'cId')
+      .select('cte_cc.cId', 'cId')
       .addSelect('SUM(ccp.payment_amount)', 'pa')
-      .from(cctSubQuery.getQuery(), 't')
-      .leftJoin(
-        'collaboration_company_payment',
-        'ccp',
-        'ccp.company_id = t.ccId',
-      )
-      .groupBy('t.cId');
+      .from('cte_cc', 'cte_cc')
+      .leftJoin(CollaborationCompanyPayment, 'ccp', 'ccp.company_id=cte_cc.cId')
+      .groupBy('cte_cc.cId');
 
-    const result = this.dataSource.manager
+    const resultQuery = this.dataSource
       .createQueryBuilder()
+      .addCommonTableExpression(contractQuery, 'cct_c')
+      .addCommonTableExpression(companyQuery, 'cte_cc')
       .select('A.cId', 'cId')
       .addSelect('A.ia', 'ia')
       .addSelect('B.pa', 'pa')
-      .from(invoiceSubQuery.getQuery(), 'A')
-      .leftJoin(paymentSubQuery.getQuery(), 'B', 'A.cId = B.cId');
-    console.log(result.getQuery());
+      .from(InvoiceQuery.getQuery(), 'A')
+      .leftJoin(PaymentQuery.getQuery(), 'B', 'A.cId=B.cId');
 
-    const list: object[] = await result.getRawMany();
+    console.log(resultQuery.getQuery());
+
+    const list: object[] = await resultQuery.getRawMany();
 
     return list;
   }
